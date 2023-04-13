81a848a1b2d6142ee8f18cfffcc67af5907d94fa
Ahmed Rajgoli Shoaib Shakeel <69170260+shoaibsr7@users.noreply.github.com>
2023-04-12 23:10:26 -0300

    commit message

---
+++ Lab/Lab3/StudentRecord.java
@@ -0,0 +1,43 @@
+public class StudentRecord {
+    private String firstName;
+    private String lastName;
+    private int bannerID;
+
+    public StudentRecord(){}
+    public StudentRecord(String firstName, String lastName, int bannerID) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.bannerID = bannerID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getBannerID() {
+        return bannerID;
+    }
+
+    public void setBannerID(int bannerID) {
+        this.bannerID = bannerID;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentRecord{" + "firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' +
+                ", bannerID=" + bannerID + '}';
+    }
+
+}
